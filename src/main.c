8930a582b506356e89c7b7e91f927d08448f657b
sismoke <mpavelek@gmail.com>
2016-10-11 14:36:39 +0200

    newline cleanup

--- src/main.c
+++ src/main.c
@@ -49,71 +49,47 @@
 void adc_init (void)
 
 {
-
 	GPIO_InitTypeDef GPIO_InitStructure;
-
 	ADC_InitTypeDef ADC_InitStructure;
 
 	/* Enable GPIO clock */
-
 	RCC_AHBPeriphClockCmd (RCC_AHBPeriph_GPIOA, ENABLE); //Opraviť a upraviť
 
 	/* Configure ADCx Channel 2 as analog input */
-
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
-
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-
 	GPIO_Init (GPIOA, &GPIO_InitStructure);
 
 	/* Enable the HSI oscillator */
-
 	RCC_HSICmd (ENABLE);
 
 	/* Check that HSI oscillator is ready */
-
 	while (RCC_GetFlagStatus (RCC_FLAG_HSIRDY) == RESET)
 		;
 
 	/* Enable ADC clock */
-
 	RCC_APB2PeriphClockCmd (RCC_APB2Periph_ADC1, ENABLE);
 
 	/* Initialize ADC structure */
-
 	ADC_StructInit (&ADC_InitStructure);
 
 	/* ADC1 configuration */
-
 	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
-
 	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
-
 	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-
 	ADC_InitStructure.ADC_NbrOfConversion = 1;
-
 	ADC_Init (ADC1, &ADC_InitStructure);
 
 	/* ADCx regular channel8 configuration */
-
 	ADC_RegularChannelConfig (ADC1, ADC_Channel_1, 1, ADC_SampleTime_16Cycles);
 
 	/* Enable the ADC */
-
 	ADC_Cmd (ADC1, ENABLE);
 
 	/* Wait until the ADC1 is ready */
-
-	while (ADC_GetFlagStatus (ADC1, ADC_FLAG_ADONS) == RESET)
-
-		{
-
-		}
+	while (ADC_GetFlagStatus (ADC1, ADC_FLAG_ADONS) == RESET){}
 
 	ADC_SoftwareStartConv (ADC1);
 }
